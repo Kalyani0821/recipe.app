db88790f099e9754003188bfadd0ae3a3e6caa93
Kalyani0821 <kalyaniyv21@gmail.com>
2024-07-30 15:28:12 -0400

    resolved the database issue

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,21 @@
+"""
+Tests for models.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTest(TestCase):
+    """Test models."""
+
+    def test_create_user_with_email_successful(self):
+        """test creating a user with an email is successful."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertDictEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
